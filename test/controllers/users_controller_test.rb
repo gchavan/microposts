28a919f7896329c652b229378c6988d27178b75a
gchavan <gc.zenpayroll@gmail.com>
2015-10-22 22:08:15 -0700

    Code to follow and unfollow users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -64,4 +64,14 @@ class UsersControllerTest < ActionController::TestCase
       delete :destroy, id: @other_user
     end
   end
+
+  test "should redirect following when not logged in" do
+    get :following, id: @user
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get :followers, id: @user
+    assert_redirected_to login_url
+  end
 end
